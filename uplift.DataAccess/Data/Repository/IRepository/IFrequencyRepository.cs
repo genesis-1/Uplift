abd249e972a5d3f06f929553b2b6500b7bed1965
patrick ntwali <patrick_ntwari@yahoo.com>
2021-04-16 11:29:54 +0200

    PNT: implemented views and controllers.

--- uplift.DataAccess/Data/Repository/IRepository/IFrequencyRepository.cs
+++ uplift.DataAccess/Data/Repository/IRepository/IFrequencyRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using uplift.DataAccess.Repository.IRepository;
@@ -8,6 +9,8 @@ namespace uplift.DataAccess.Data.Repository.IRepository
 {
     public interface IFrequencyRepository: IRepositroy<Frequency>
     {
+        IEnumerable<SelectListItem> GetFrequencyListForDropDown();
 
+        void Update(Frequency frequency);
     }
 }
